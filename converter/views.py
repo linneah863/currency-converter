f78bf1ae09d118b133c5ce14eef7e7a792bc3853
Alina <alina-petrovich@tut.by>
2018-12-04 19:25:53 +0300

    last changes

--- converter/views.py
+++ converter/views.py
@@ -4,19 +4,24 @@ from django.views import View
 from django.views.generic import TemplateView
 
 from converter.forms import BalanceForm
-from converter.models import ExchangeRate
+from converter.models import ExchangeRate, Currency
 
 
 def convert(request):
     if request.method == "POST":
         form = BalanceForm(request.POST)
         if form.is_valid():
-            rate = ExchangeRate.objects.get(o_currency=form.cleaned_data['o_currency'], t_currency=form.cleaned_data['t_currency'])
-        balanse = form.cleaned_data['balance']
-        result = float(balanse)*float(rate)
-        return HttpResponseRedirect('')
+            rate = ExchangeRate.objects.get(o_currency=form.cleaned_data['o_currency'], t_currency=form.cleaned_data['t_currency']).rate
+            form.clean()
+        balance = form.cleaned_data['balance']
+        result = float(balance)/float(rate)
+        result = round(result, 2)
+        currency = Currency.objects.all()
+        return render(request, 'base.html', {'currency': currency, 'result': result})
     else:
-        return render(request, 'base.html', {'form': BalanceForm()})
+        currency = Currency.objects.all()
+        result = None
+        return render(request, 'base.html', {'currency': currency, 'result': result})
 
 
 
